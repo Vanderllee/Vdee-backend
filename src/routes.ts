4b7505a38408fd20759ebf111cd0fc200eccfa50
Vanderllee <vanderllee@gmail.com>
2021-07-27 15:32:07 -0300

    routes

---
+++ src/routes.ts
@@ -0,0 +1,38 @@
+import { Router } from "express";
+
+import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/createComplimentController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
+
+const router = Router();
+
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
+
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
+
+const listTagsController = new ListTagsController();
+
+const authenticateUserController = new AuthenticateUserController();
+
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
+router.get("/tags", ensureAuthenticated, listTagsController.handle);
+
+router.post("/users", createUserController.handle);
+router.post("/signin", authenticateUserController.handle);
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle);
+router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle);
+
+
+
+export { router };
